a45fa689ae3ae2bcad6f87b55b7d5b0a2f540e08
shigapov <57352291+shigapov@users.noreply.github.com>
2021-03-05 16:55:43 +0100

    set name to RaiseWikibase

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="bbw",
+    name="RaiseWikibase",
     author="Renat Shigapov, Joerg Mechnich, Irene Schumm",
     license="MIT",
     description="RaiseWikibase: Fast inserts into a Wikibase instance",
