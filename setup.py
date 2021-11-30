1e425e10d8aab8872e6dccf71d03c6aa363d7397
Renat Shigapov <57352291+shigapov@users.noreply.github.com>
2021-11-30 19:42:31 +0100

    set v1.35.0-wmde.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="RaiseWikibase",
-    version='0.1.0',
+    version='1.35.0-wmde.0',
     author="Renat Shigapov, Joerg Mechnich, Irene Schumm",
     license="MIT",
     description="RaiseWikibase: Fast inserts into a Wikibase instance",
